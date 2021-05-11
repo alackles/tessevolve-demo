6bc58767a90024cd4904cc61b652aa0d3cfc35fb
Acacia Ackles <alackles@msu.edu>
2021-05-11 15:04:07 -0400

    stub file for python implementations of niching fcns and their names

---
+++ source/fcn_outputs.py
@@ -0,0 +1,5 @@
+# import functions from niching space
+from niching.python3.cec2013.functions import shubert
+from niching.python3.cec2013.functions import vincent
+from niching.python3.cec2013.CF3 import CF3
+from niching.python3.cec2013.CF4 import CF4
