99de31edd52037cfff31fd2a21728800a224c099
Acacia Ackles <alackles@msu.edu>
2021-09-07 08:39:58 -0400

    Move shubert to new file

---
+++ source/aframe/landscape.js
@@ -0,0 +1,46 @@
+var accessor = function(row) {
+    return {
+        x: +row.x*10,
+        y: +row.y*10,
+        z: +row.z*10,
+        fitness: +row.fitness
+    }
+}
+
+var coords = function(x, y, z) {
+    return x + " " + y + " " + z
+}
+
+var load_landscape = function(filename, min, max) {
+
+    var scene = d3.select('a-scene')
+
+    var rScale = d3.scaleSqrt();
+    rScale.domain([min, max]).range([0, 5]);
+
+    var opScale = d3.scaleSqrt();
+    opScale.domain([min, max]).range([0.25, 0.75]);
+
+
+    d3.csv(filename, accessor)
+    .then(
+        function(landscape) {
+
+        var pts = scene.selectAll('a-sphere')
+            .data(landscape, function(d){return d.x})
+
+        pts.enter()
+            .append('a-sphere')
+            .attr('class', 'data_point')
+            .attr('color', 'darkseagreen')
+            .attr('transparent', 'true')
+            .attr('position', function(d) {return coords(d.x, d.y, d.z)})
+            .attr('radius', function(d) {return rScale(d.fitness)})
+            .attr('opacity', function(d) {return opScale(d.fitness)});
+        }
+    )
+
+}
+
+
+
--- source/aframe/shubert.js
+++ source/aframe/shubert.js
@@ -1,40 +1,12 @@
-var scene = d3.select('a-scene')
+$.getScript("./landscape.js", function()
+    {;
+        fname = "../../data/coords_shubert.csv";
+        fmin = 0;
+        fmax = 100;
 
-var accessor = function(row) {
-    return {
-        x: +row.x*10,
-        y: +row.y*10,
-        z: +row.z*10,
-        fitness: +row.fitness
+        load_landscape(fname, fmin, fmax);
     }
-}
 
-var coords = function(x, y, z) {
-    return x + " " + y + " " + z
-}
+)
 
-var rScale = d3.scaleSqrt();
-rScale.domain([0, 100]).range([0, 5]);
 
-var opScale = d3.scaleSqrt();
-opScale.domain([0, 100]).range([0.25, 0.75]);
-
-d3.csv("../../data/coords_shubert.csv", accessor)
-    .then(
-        function(landscape) {
-
-        var pts = scene.selectAll('a-sphere')
-            .data(landscape, function(d){return d.x})
-
-        pts.enter()
-            .append('a-sphere')
-            .attr('class', 'data_point')
-            .attr('color', 'darkseagreen')
-            .attr('transparent', 'true')
-            .attr('position', function(d) {return coords(d.x, d.y, d.z)})
-            .attr('radius', function(d) {return rScale(d.fitness)})
-            .attr('opacity', function(d) {return opScale(d.fitness)});
-
-        console.log(coords(10, 2, 4));
-        }
-    )
