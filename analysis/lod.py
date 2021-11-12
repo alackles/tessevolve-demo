ee24d53e45736312c1d281b17c99bfce7d6740f3
Acacia Ackles <alackles@msu.edu>
2021-11-12 13:17:52 -0500

    Remove alife-std-dev as a submodule

--- analysis/lod.py
+++ analysis/lod.py
@@ -6,4 +6,5 @@ import matplotlib.pyplot as plt
 filename = "../data/reps/SEED_00__F_CF3/phylogeny_1000.csv"
 
 phylo = phylodev.load_phylogeny_to_networkx(filename)
-print(phylodev.get_leaf_taxa_ids(phylo))
+lod = phylodev.extract_asexual_lod(phylo, not_destroyed_value="inf")
+print(lod)
