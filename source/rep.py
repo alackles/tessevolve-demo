99f2e82d21882200816088d23d84431f6d84537c
Acacia Ackles <alackles@msu.edu>
2022-02-09 10:02:39 -0500

    Add support for 2d

--- source/rep.py
+++ source/rep.py
@@ -21,18 +21,19 @@ mabefile = "./third-party/MABE2/settings/GECCO.mabe"
 firstrep = 0
 lastrep = 1
 fcns = {
-  "Shubert": (-5, 5), 
-  "Vincent": (0.25, 10.25), 
-  "CF3": (-5, 5), 
+#  "Shubert": (-5, 5), 
+#  "Vincent": (0.25, 10.25), 
+#  "CF3": (-5, 5), 
   "CF4": (-5, 5)
   }
-mutrates = [0.1, 0.01, 0.001, 0.0001]
 tournament_sizes = [2, 4, 8, 16]
+mutrates = [0.1, 0.01, 0.001, 0.0001]
+dims = [2, 3]
 #digs = len(str(lastrep-firstrep))
 digs = 2
 
 # clean build of MABE
-os.system("cd " + buildpath + "&& make clean && make && cd ../../../")
+#os.system("cd " + buildpath + "&& make clean && make && cd ../../../")
 
 for fcn, minmax in fcns.items():
   minval = minmax[0]
@@ -41,30 +42,33 @@ for fcn, minmax in fcns.items():
     tournystr = str(tourny).rjust(2, '0')
     for mut in mutrates:
       mutstr = str(mut).replace(".","")
-      for rep in range(firstrep, lastrep):
-          # random seeds and folder creation
-          randseed = rep
-          dirname = reppath + "SEED_" + str(randseed).rjust(digs, '0') + "__F_" + fcn + "__MUT_" + mutstr + "__T_" + tournystr + "/" 
-          print(dirname)
-          Path(dirname).mkdir(parents=True, exist_ok=True)
+      for dim in dims:
+        dimstr = str(dim)
+        for rep in range(firstrep, lastrep): 
+            # random seeds and folder creation
+            randseed = rep
+            dirname = reppath + "SEED_" + str(randseed).rjust(digs, '0') + "__F_" + fcn + "__D_" + dimstr + "__MUT_" + mutstr + "__T_" + tournystr + "/" 
+            print(dirname)
+            Path(dirname).mkdir(parents=True, exist_ok=True)
 
-          #variables 
-          fcn_var = 'eval_gecco.fcn_name=\\"' + fcn + '\\"'
-          min_var = 'vals_org.min_value=\\"' + str(minval) + '\\"'
-          max_var = 'vals_org.max_value=\\"' + str(maxval) + '\\"'
+            #variables 
+            fcn_var = 'eval_gecco.fcn_name=\\"' + fcn + '\\"'
+            dim_var = 'eval_gecco.dims=\\"' + str(dim) + '\\"'
+            min_var = 'vals_org.min_value=\\"' + str(minval) + '\\"'
+            max_var = 'vals_org.max_value=\\"' + str(maxval) + '\\"'
 
-          mut_var = 'vals_org.mut_size=\\"' + str(mut) + '\\"'
-          tourney_var = 'select_tourny.tournament_size=\\"' + str(tourny) + '\\"'
+            mut_var = 'vals_org.mut_size=\\"' + str(mut) + '\\"'
+            tourney_var = 'select_tourny.tournament_size=\\"' + str(tourny) + '\\"'
 
-          randseed_var = "random_seed=" + str(randseed)
-          datpath_var = 'eval_gecco.dat_path=\\"' + datpath + '\\"'
+            randseed_var = "random_seed=" + str(randseed)
+            datpath_var = 'eval_gecco.dat_path=\\"' + datpath + '\\"'
 
-          outpath_var = 'output.filename=\\"' + dirname + 'output.csv\\"'
-          genpath_var = 'eval_gecco.genome_file=\\"' + dirname + 'genome.csv\\"'
-          phylopath_var = 'sys.snapshot_file_root_name=\\"' + dirname + 'phylogeny\\"'
-          phylodatapath_var = 'sys.data_file_name=\\"' + dirname + 'phylogenetic_data.csv\\"'
+            outpath_var = 'output.filename=\\"' + dirname + 'output.csv\\"'
+            genpath_var = 'eval_gecco.genome_file=\\"' + dirname + 'genome.csv\\"'
+            phylopath_var = 'sys.snapshot_file_root_name=\\"' + dirname + 'phylogeny\\"'
+            phylodatapath_var = 'sys.data_file_name=\\"' + dirname + 'phylogenetic_data.csv\\"'
 
 
-          settings = fcn_var + "\;" + min_var + "\;" + max_var + "\;" + mut_var + "\;" + tourney_var +"\;" + randseed_var + "\;" + datpath_var + "\;" + outpath_var + "\;" + genpath_var + "\;" + phylopath_var + "\;" + phylodatapath_var 
-          print(settings)
-          os.system(runpath + " -f " + mabefile + " -s " + settings)
+            settings = fcn_var + "\;" + dim_var + "\;" + min_var + "\;" + max_var + "\;" + mut_var + "\;" + tourney_var +"\;" + randseed_var + "\;" + datpath_var + "\;" + outpath_var + "\;" + genpath_var + "\;" + phylopath_var + "\;" + phylodatapath_var 
+            print(settings)
+            os.system(runpath + " -f " + mabefile + " -s " + settings)
