10907a9ef02534a28119d3b07ed16e20b48e68df
Acacia Ackles <alackles@msu.edu>
2022-02-09 13:30:42 -0500

    Refactor LOD gen

--- analysis/lod.py
+++ analysis/lod.py
@@ -3,55 +3,74 @@ import networkx as nx
 import math
 import csv
 import collections
-import os
 from decimal import Decimal
 
-datapath = "./../data/"
-reppath = datapath + "reps/"
 
-firstrep = 0
-lastrep = 1
-fcns = ["Shubert", "Vincent", "CF3", "CF4"]
-mutrates = ["01", "001", "0001", "00001"]
-tournament_sizes = [2, 4, 8, 16]
-digs = len(str(lastrep-firstrep))
+def lod_dict_from_phylo(filepath):
+    # create ordered dictionary from lod
+    phylo = phylodev.load_phylogeny_to_networkx(filepath)
+    extant_ids = phylodev.get_extant_taxa_ids(phylo, not_destroyed_value=math.inf)
+    lod = phylodev.extract_asexual_lineage(phylo,extant_ids[0])
+    lod_dict = dict(lod.nodes(data="taxon_info"))
+    lod_chrono = collections.OrderedDict(sorted(lod_dict.items()))
+    return lod_chrono
 
-filename = "phylogeny_1000.csv"
-lodname = "lod.csv"
-genname = "genome.csv"
-edgename = "edges.csv"
+def export_lod(lod, dims, lodpath="lod.csv"):
+    # create line of descent: lod.csv
+    with open(lodpath, "w") as lod_file:
+        if (dims==3):
+            lod_file.write("id,x,y,z\n")
+        elif (dims==2):
+            lod_file.write("id,x,y\n")
+        else:
+            print("Dims invalid")
+            return 0
+    for gen, vals in lod.items():
+        vals = ",".join(vals.lstrip('[ ').rstrip(' ]').split())
+        line = str(gen) + "," + vals + "\n"
+        with open(lodpath, "a") as lod_file:
+            lod_file.write(line) 
 
-for fcn in fcns:
-    for mut in mutrates:
-        for t in tournament_sizes:
-            for rep in range(firstrep, lastrep):
-                dirpath = reppath + "SEED_" + str(rep).rjust(2, '0') + "__F_" + fcn + "__MUT_" + mut + "__T_" + str(t).rjust(2, '0') + "/" 
-                filepath = dirpath + filename
-                lodpath = dirpath + lodname 
-                edgepath = dirpath + edgename
+def export_edges(lod, edgepath="edges.csv"):
+    # create single string for edges for meshline: edges.csv
+    edge_list = []
+    for vals in lod.values():
+        # multiply string values by 10 and convert back to string
+        vals = " ".join([str(Decimal(x)*10) for x in vals.lstrip('[ ').rstrip(' ]').split()])
+        edge_list.append(vals)
+    with open(edgepath, "w") as edge_file:
+        edge_file.write(",".join(edge_list))
 
-                phylo = phylodev.load_phylogeny_to_networkx(filepath)
-                extant_ids = phylodev.get_extant_taxa_ids(phylo, not_destroyed_value=math.inf)
-                lod = phylodev.extract_asexual_lineage(phylo, extant_ids[0])
+def lod_gen(dims, first=0, last=10):
 
-                # create line of descent: lod.csv
-                lod_dict = dict(lod.nodes(data="taxon_info"))
-                lod_chrono = collections.OrderedDict(sorted(lod_dict.items()))
-                with open(lodpath, "w") as lod_file:
-                    lod_file.write("id,x,y,z\n")
-                for gen, vals in lod_chrono.items():
-                    vals = ",".join(vals.lstrip('[ ').rstrip(' ]').split())
-                    line = str(gen) + "," + vals + "\n"
-                    with open(lodpath, "a") as lod_file:
-                        lod_file.write(line) 
+    datapath = "./../data/"
+    reppath = datapath + "reps/"
 
-                # create single string for edges for meshline: edges.csv
-                edge_list = []
-                for vals in lod_chrono.values():
-                    # multiply string values by 10 and convert back to string
-                    vals = " ".join([str(Decimal(x)*10) for x in vals.lstrip('[ ').rstrip(' ]').split()])
-                    edge_list.append(vals)
-                with open(edgepath, "w") as edge_file:
-                    edge_file.write(",".join(edge_list))
+    fcns = ["Shubert", "Vincent", "CF3", "CF4"]
+    mutrates = ["01", "001", "0001", "00001"]
+    tournament_sizes = [2, 4, 8, 16]
+    digs = len(str(last-first))
+
+    filename = "phylogeny_1000.csv"
+    lodname = "lod.csv"
+    genname = "genome.csv"
+    edgename = "edges.csv"
+
+    for fcn in fcns:
+        for mut in mutrates:
+            for t in tournament_sizes:
+                for rep in range(first, last):
+                    dirpath = reppath + "SEED_" + str(rep).rjust(2, '0') + "__F_" + fcn + "__D_" + str(dims) + "__MUT_" + mut + "__T_" + str(t).rjust(2, '0') + "/" 
+                    filepath = dirpath + filename
+                    lodpath = dirpath + lodname 
+                    edgepath = dirpath + edgename
+
+                    lod = lod_dict_from_phylo(filepath)
+                    export_lod(lod, dims, lodpath)
+                    export_edges(lod, edgepath)
+
+lod_gen(2)
+lod_gen(3)
+
 
 
