e3049fcda142790e801b2a179b731a1e74ee099f
Acacia Ackles <alackles@msu.edu>
2022-03-24 09:18:35 -0400

    Adjust naming

--- source/scripts/edge_gen.py
+++ source/scripts/edge_gen.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 from repparse import parameters
 # Turn the line of descent file into a single string for the purposes of creating a "meshline"
 # Format is val1, val2, [val3, val4]; val1, val2, val3, val4;
@@ -8,24 +6,19 @@ def export_edges(lodpath="lod.csv", edgepath="edges.csv"):
     with open(lodpath, "r") as lod_file:
         next(lod_file)
         for line in lod_file:
-            edge_list.append(" ".join(x for x in line.split(",")[1:-1])) # grab middle values, not fitness or ID
+            edge_list.append(" ".join(x for x in line.split(",")[1:4])) # grab x0, x1, x2
     with open(edgepath, "w") as edge_file:
         edge_file.write(",".join(edge_list))
 
 def main():
 
-    lodnames = ["lod_full.csv", "lod_hi.csv", "lod_md.csv", "lod_lo.csv"]
-
     params = parameters()
 
     for p in params:
         dirpath = p["path"]
-        lodpaths = [dirpath + x for x in lodnames]
-
-        for lod in lodpaths:
-            edge = lod.replace("lod", "edge")
-            export_edges(lod, edge)
-
+        lodpath = dirpath + "lod.csv"
+        edgepath = lodpath.replace("lod", "edge")
+        export_edges(lodpath, edgepath)
         print(dirpath)
 
 if __name__ == "__main__":
