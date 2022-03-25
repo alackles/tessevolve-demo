bddce66653f44ede15850d06e4c440aaf30f682c
Acacia Ackles <alackles@msu.edu>
2022-03-25 09:12:18 -0400

    Change JS basepath for web

--- source/js/main.js
+++ source/js/main.js
@@ -49,7 +49,8 @@ var load_landscape = function() {
     var tourny = document.querySelector('select[name="tour_size"]').value;
     var phylo_detail = document.querySelector('select[name="phylo"]').value;
 
-    var basepath = "../../data/";
+    var basepath = "../../tessevolve/data/"; // for web deployment
+    // var basepath = "../../data/" // for local host 
     var coord_data = basepath + "coords/coords_" + fcn + "_" + dim + "D.csv";
 
     var replicate_path = basepath + "reps/SEED_" + seed + "__F_" + fcn + "__D_" + dim + "__MUT_" + mutrate + "__T_" + tourny + "/";
